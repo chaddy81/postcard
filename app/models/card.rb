9c3b066195a80620f2d29c32752236800743bc09
Chad Bartels <chad.bartels@gmail.com>
2011-10-15 19:02:39 -0400

    .

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,9 @@
 class Card
   include Mongoid::Document
-  include Mongoid::Paperclip
   field :name, :type => String
   field :message, :type => String
 
-  has_mongoid_attached_file :photo,
+  has_attached_file :photo,
        :styles => {
        :thumb=> "200x200#",
        :small  => "400x400#" },
